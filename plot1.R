212aef46a33c5d3f6dc6b686e71ef46ce54c0097
HCMNell <hcm.nell@gmail.com>
2017-07-22 16:46:20 +0200

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,8 @@
+colnames <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+a <- grep("1/2/2007", readLines("household_power_consumption.txt"))[1]
+b <- grep("3/2/2007", readLines("household_power_consumption.txt"))[1]
+data <- read.table("household_power_consumption.txt",col.names=colnames,sep=";",skip=a-1,na.strings="?",nrows=(b-a))
+data[[1]] <- strptime(paste(data[[1]],data[[2]]),format="%d/%m/%Y %H:%M:%S")
+png(file="plot1.png")
+hist(data[[3]], col="red", xlab="Global Active Power (kilowatts)",main="Global Active Power")
+dev.off()
